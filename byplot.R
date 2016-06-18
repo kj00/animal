0307d374cad0f77f49749197721cb0f44caab665
kj00 <kh03@outlook.com>
2016-06-18 21:37:31 +0900

    adding histogram plots and modifications

--- byplot.R
+++ byplot.R
@@ -2,21 +2,43 @@
 
 library(ggplot2)
 
-##dt = data.table, count = main varible you want to count, byval = variable you want to categorize "count"
+##dt = data.table, outcome = main varible you want to count, byval = variable you want to categorize "count"
 ##inv = if T, change order
-byplot <- function(dt, count, byval, stat = "identity", inv = F) {
+byplot <- function(dt, outcome, byval, position = "stack", inv = F, type = "bar", binwidth = 100) {
   
   if (inv == T) {
     
-    ptr1 <- train[, lapply(.SD, length), by = c(count, byval)][order(get(byval))]
+    ptr1 <- dt[, .N, by = c(outcome, byval)][order(get(byval))]
     
   }   else {
-    ptr1 <- train[, lapply(.SD, length), by = c(count, byval)][order(-get(byval))]
+    ptr1 <- dt[, .N, by = c(outcome, byval)][order(-get(byval))]
   }
   
-  ggptr1 <- ggplot(ptr1, aes_string(x = count, y = "AnimalID"))
   
-  plot <- ggptr1 + geom_bar(stat = stat) + aes_string(fill = byval)
   
+  if (type == "bar") {
+  
+    ggptr1 <- ggplot(ptr1, aes_string(x = outcome, y = "N", fill = byval))
+    
+    plot <- ggptr1 + geom_bar(stat = "identity", position = position)
+  
+   }
+
+  if (type == "freq") {
+  
+    ggptr1 <- ggplot(ptr1, aes_string(x = "N", color = byval)) 
+
+    plot <- ggptr1 + geom_freqpoly(binwidth = binwidth)
+    
+  }
+  
+  if (type == "hist") {
+    
+    ggptr1 <- ggplot(ptr1, aes_string(x = "N", fill = byval)) 
+    
+    plot <- ggptr1 + geom_histogram(binwidth = binwidth)
+    
+  }
   return(plot)    
-}
+  
+  }
