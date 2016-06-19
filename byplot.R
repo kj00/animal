1feb675865dd30c2d39189ae5dfa765065350807
kj00 <kh03@outlook.com>
2016-06-19 17:12:28 +0900

    modify expression and add "line" and "point plots.

--- byplot.R
+++ byplot.R
@@ -1,44 +1,59 @@
 ##Plot categorized data.table by bar ggplot
 
 library(ggplot2)
-
+library(reshape2)
 ##dt = data.table, outcome = main varible you want to count, byval = variable you want to categorize "count"
 ##inv = if T, change order
 byplot <- function(dt, outcome, byval, position = "stack", inv = F, type = "bar", binwidth = 100) {
   
   if (inv == T) {
     
-    ptr1 <- dt[, .N, by = c(outcome, byval)][order(get(byval))]
+    subdt <- dt[, .N, by = c(outcome, byval)][order(get(byval))]
     
   }   else {
-    ptr1 <- dt[, .N, by = c(outcome, byval)][order(-get(byval))]
+    subdt <- dt[, .N, by = c(outcome, byval)][order(-get(byval))]
   }
   
   
   
   if (type == "bar") {
   
-    ggptr1 <- ggplot(ptr1, aes_string(x = outcome, y = "N", fill = byval))
+    ggdt <- ggplot(subdt, aes_string(x = outcome, y = "N", fill = byval))
     
-    plot <- ggptr1 + geom_bar(stat = "identity", position = position)
+    plot <- ggdt + geom_bar(stat = "identity", position = position)
   
    }
 
   if (type == "freq") {
   
-    ggptr1 <- ggplot(ptr1, aes_string(x = "N", color = byval)) 
+    ggdt <- ggplot(subdt, aes_string(x = "N", color = byval)) 
 
-    plot <- ggptr1 + geom_freqpoly(binwidth = binwidth)
+    plot <- ggdt + geom_freqpoly(binwidth = binwidth)
     
   }
   
   if (type == "hist") {
     
-    ggptr1 <- ggplot(ptr1, aes_string(x = "N", fill = byval)) 
+    ggdt <- ggplot(subdt, aes_string(x = "N", fill = byval)) 
     
-    plot <- ggptr1 + geom_histogram(binwidth = binwidth)
+    plot <- ggdt + geom_histogram(binwidth = binwidth)
     
   }
+  
+  if (type == "line"){
+    
+    ggdt <- ggplot(subdt, aes_string(x = byval))
+    plot <- ggdt + geom_line(aes_string(y = "N", colour = outcome))
+    
+  }
+
+  if (type == "point"){
+    
+    ggdt <- ggplot(subdt, aes_string(x = byval))
+    plot <- ggdt + geom_point(aes_string(y = "N", colour = outcome))
+    
+  }
+  
   return(plot)    
   
   }
